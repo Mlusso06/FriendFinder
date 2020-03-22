710c9786c28ed9faa44a4fe0445f7f0c038dec0b
spe1901 <52164194+Mlusso06@users.noreply.github.com>
2020-03-21 22:44:42 -0500

    adding my friends to my database

--- app/data/friends.js
+++ app/data/friends.js
@@ -1,16 +1,34 @@
-{
-    "name":"Ahmed",
-    "photo":"https://media.licdn.com/mpr/mpr/shrinknp_400_400/p/6/005/064/1bd/3435aa3.jpg",
-    "scores":[
-        5,
-        1,
-        4,
-        4,
-        5,
-        1,
-        2,
-        5,
-        4,
-        1
-      ]
-  }
+var friends = [
+    {
+        "name": "Jake from State Farm",
+        "photo": "https://a.wattpad.com/cover/64591564-352-k956444.jpg",
+        "scores": [5, 1, 4, 4, 5, 1, 2, 5, 4, 1 ]
+    },
+    {
+        "name": "BudLight Knight",
+        "photo": "http://www.cgw.com/images/cmssys/thumbnails/BudLightHBO580-580345-13-TransparentWhite-15235-580345-13-TransparentWhite-1.jpg",
+        "scores": [5, 4, 2, 3, 1, 3, 1, 4, 4, 2 ]
+    },
+    {
+        "name": "Buddy the Elf",
+        "photo": "https://www.awesomeinventions.com/wp-content/uploads/2019/10/buddy.jpg",
+        "scores": [4, 3, 4, 2, 4, 3, 5, 3, 5, 4 ]
+    },
+    {
+        "name": "Mark Wahlberg",
+        "photo": "https://www.rollingstone.com/wp-content/uploads/2018/06/mark-wahlberg-boogie-nights-1ff3f595-8136-4e2f-a31a-edcaf6413535.jpg?resize=1800,1200&w=1200",
+        "scores": [4, 4, 2, 3, 2, 2, 3, 2, 4, 5 ]
+    },
+    {
+        "name": "Tom From MYspace",
+        "photo": "http://hackaday.com/wp-content/uploads/2008/08/tomanderson.jpg",
+        "scores": [2, 3, 3, 1, 5, 3, 5, 1, 3, 1 ]
+    },
+    {
+        "name": "Big Jay - Kansas Jayhawk",
+        "photo": "https://espnpressroom.com/us/files/2018/02/RS562652_20171114_PE3_6710-e1518622066935-780x405.jpg",
+        "scores": [3, 5, 3, 2, 5, 5, 3, 2, 1, 3 ]
+    }
+];
+module.exports = friends;
+
--- server.js
+++ server.js
@@ -2,25 +2,23 @@
 // =============================================================
 var express = require("express");
 var path = require("path");
+var bodyParser = require("body-parser");
+require("dotenv").config();
 
 
 // Sets up the Express App
 // =============================================================
 var app = express();
-var PORT = 3000;
+var PORT = process.env.PORT;
 
 // Sets up the Express app to handle data parsing
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
 
-
-
-
-
-
-
-
+// set up the router requirments
+require("./app/data/routing/apiRoutes")(app);
+require("./app/data/routing/htmlRoutes")(app);
 
 
 // Starts the server to begin listening
